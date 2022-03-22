e508757513e92e1e96d86adaaff8530db51abced
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-03-22 22:55:33 +0100

    prettier testing

--- src/Components/Main/Introduction/Description/index.js
+++ src/Components/Main/Introduction/Description/index.js
@@ -1,12 +1,12 @@
 import { Wrapper } from './styled';
 
 const Description = () => {
-	return (
-		<Wrapper>
-			<h1>Miłosz</h1>
-			<p>dev</p>
-		</Wrapper>
-	);
+ return (
+  <Wrapper>
+   <h1>Miłosz</h1>
+   <p>dev</p>
+  </Wrapper>
+ );
 };
 
 export default Description;
