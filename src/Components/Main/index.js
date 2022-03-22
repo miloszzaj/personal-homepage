0a056a29c0912035a1fbd10e323ecb35e02de976
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-03-22 22:56:17 +0100

    prettier testing

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -2,11 +2,11 @@ import Introduction from './Introduction';
 import { Wrapper } from './styled';
 
 const Main = () => {
-	return (
-		<Wrapper>
-			<Introduction />
-		</Wrapper>
-	);
+ return (
+  <Wrapper>
+   <Introduction />
+  </Wrapper>
+ );
 };
 
 export default Main;
