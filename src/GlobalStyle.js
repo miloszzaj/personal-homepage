595eaa4e9ba7365f031e46ca98b9c8934ca1b311
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-04-04 21:26:13 +0200

    transition in theme changing

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
 html {
   box-sizing: border-box;
   font-family: 'Inter', sans-serif;
+  transition: all 0.9s linear;
   background-color: ${({ theme }) => theme.background.primary};
 }
 
