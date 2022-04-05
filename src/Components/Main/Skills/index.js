d17705d2a185bd545d1ab4887efdb81313330828
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-04-05 11:23:28 +0200

    skills to assets moved

--- src/Components/Main/Skills/index.js
+++ src/Components/Main/Skills/index.js
@@ -1,17 +1,6 @@
 import { ListItem, ListWrapper } from '../../Section/styled';
-const skillListArray = [
-  'HTML',
-  'Responsive Web Design',
-  'Teamwork',
-  'Markdown',
-  'CSS BEM convention',
-  'CSS Grid',
-  'CSS FlexBox',
-  'React Router',
-  'Redux-Saga',
-  'Redux (Toolkit)',
-  'React Hooks',
-];
+import { skillListArray } from '../../../assets/data/skills';
+
 const Skills = () => {
   return skillListArray.map(skill => (
     <ListWrapper key={skill}>
